b2c3f8f241cacbb64109cbdd55f50157f575ca39
ranjubhat7 <ranjan@publicissapient.com>
2022-08-03 09:30:41 +0530

    jest installation

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
 });
